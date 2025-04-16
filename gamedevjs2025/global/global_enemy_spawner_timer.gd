415d277149fb68ad48747358b51d463884d1279c
Daq_Vid <dlim0012.gaming@gmail.com>
2025-04-15 23:12:47 -0700

    Created campfire, and created a script for the timer

--- gamedevjs2025/global/global_enemy_spawner_timer.gd
+++ gamedevjs2025/global/global_enemy_spawner_timer.gd
@@ -1,24 +1,26 @@
 extends Node
 
-@onready var peace_timer := $PeaceTimer
-@onready var enemy_timer := $EnemySpawnTimer
+# Peace timer for around 4 minutees
+# Enemy timer for around 1 minute.
+var peace_duration := 240.0 # 4 minutes
+var enemy_duration := 60.0  # 1 minute
 
 func _ready() -> void:
-	print("successful began to start the peace timer")
-	peace_timer.start()
+	print(">>> Peace begins.")
+	start_peace_timer()
 
-func _on_enemy_spawn_timer_timeout() -> void:
-	print("enemy spawn done, peace time")
-	peace_timer.start()
-	enemy_timer.stop()
+func start_peace_timer() -> void:
 	GlobalPlayerScript.is_peace_time = true
+	print("Peace time active for 4 minutes.")
+	var peace_timer := get_tree().create_timer(peace_duration)
+	peace_timer.connect("timeout", Callable(self, "_on_peace_timer_timeout"))
 
-
-func _on_peace_timer_timeout() -> void:
-	print("Peace timer gone, enemy spawn begin")
-	peace_timer.stop()
-	enemy_timer.start()
+func start_enemy_timer() -> void:
 	GlobalPlayerScript.is_peace_time = false
+	print("Combat phase active for 1 minute.")
+	var enemy_timer := get_tree().create_timer(enemy_duration)
+	enemy_timer.connect("timeout", Callable(self, "_on_enemy_timer_timeout"))
+
 
 # The recommended thought I have for this is that we use signals and connec these to the actual environment. I'm still unsure how to utilize signals, so I could
 # use the extra help with this. - Daq_Vid
