b902598dc7492ac7b2c43bcc0ce73e58b8d1f952
Daq_Vid <dlim0012.gaming@gmail.com>
2025-04-17 23:48:38 -0700

    Small changes to the fire

--- gamedevjs2025/scenes/assets/campfire/campfire.gd
+++ gamedevjs2025/scenes/assets/campfire/campfire.gd
@@ -1,8 +1,9 @@
 extends StaticBody2D
-
-func _process(delta) -> void:
+func _process(_delta) -> void:
 	# The middle is 25
+	# The middle of the scale is Vector2(18.0, 13.59)
 	$PointLight2D.energy = randf_range(24.9, 25.1)
+	$PointLight2D.scale = Vector2(randf_range(17.5, 18.5), randf_range(13.40, 14))
 
 
 func _on_campfire_interract_body_entered(body:Node2D) -> void:
