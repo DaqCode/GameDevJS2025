8e885bd07986df38e6b5f2868426117b4994d51b
Daq_Vid <dlim0012.gaming@gmail.com>
2025-04-18 14:38:48 -0700

    Put ashes

---
+++ gamedevjs2025/scenes/UI/ash_count.gd
@@ -0,0 +1,18 @@
+extends CanvasLayer
+
+@onready var amount_of_ashes := $Panel/AmountOfAshes
+@onready var ash_timer := $AshCountTimer
+
+var ash_count := 0
+
+func _ready() -> void:
+	ash_timer.start()
+
+func _process(_delta):
+	#820.0, 15.0
+	$Panel.position = Vector2(randf_range(819.25, 820.75), randf_range(14.25,15.75))
+
+func _on_ash_count_timeout() -> void:
+	ash_count+=1
+	amount_of_ashes.text = str(ash_count)
+	ash_timer.start()
