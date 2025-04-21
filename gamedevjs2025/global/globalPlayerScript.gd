9ea327c0062caaa70b63c15fb278ed5641abbaae
Daq_Vid <dlim0012.gaming@gmail.com>
2025-04-20 19:36:28 -0700

    imported different inventory

--- gamedevjs2025/global/globalPlayerScript.gd
+++ gamedevjs2025/global/globalPlayerScript.gd
@@ -1,3 +1,9 @@
 extends Node
 
 var is_peace_time : bool = true
+
+var lumb_mus_upgrade : int = 1
+var loc_eco_upgrade : int = 1
+var fur_fire_upgrade : int = 1
+var angry_mus_upgrade : int = 1
+var speed_leg_upgrade : int = 1
--- gamedevjs2025/scenes/UI/upgrade_station.gd
+++ gamedevjs2025/scenes/UI/upgrade_station.gd
@@ -7,10 +7,14 @@ var this_is_dumb := 1
 func _ready() -> void:
 	# Upgrade 1-5 exists
 	for i in button_container.get_children():
-		i.connect("pressed", Callable(self, ("_on_upgrade_pressed_%d" %this_is_dumb)))
-		i.connect("mouse_entered", Callable(self, "_on_upgrade_mouse_entered").bind(i))
-		i.connect("mouse_exited", Callable(self, "_on_upgrade_mouse_exited").bind(i))
-		this_is_dumb+=1  # Connect to speciifc... wait why didnt i just connect it from godot......
+		if i is Button:
+			i.connect("pressed", Callable(self, ("_on_upgrade_pressed_%d" %this_is_dumb)))
+			i.connect("mouse_entered", Callable(self, "_on_upgrade_mouse_entered").bind(i))
+			i.connect("mouse_exited", Callable(self, "_on_upgrade_mouse_exited").bind(i))
+			this_is_dumb+=1  # Connect to speciifc... wait why didnt i just connect it from godot......
+			
+		else:
+			print("You're not a button, ignored...")
 
 
 func _on_upgrade_pressed_1() -> void:
@@ -29,6 +33,10 @@ func _on_upgrade_pressed_5() -> void:
 	print("This is button 5 i swear to god dude.")
 
 func _on_upgrade_mouse_entered(button: Button) -> void:
+	match button:
+		%Upgrade1:
+			print("ASBIUIDSUIFGIFSYGFIUYSGFYUSGFIUK")
+
 	var tween = create_tween()
 	tween.tween_property(button, "scale", Vector2(1.1,1.1), 0.15).set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_OUT)
 
